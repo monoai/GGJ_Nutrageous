89c7518d64406e8cb7bf12cd75c3d14eed27322c
monoAI <46586720+monoai@users.noreply.github.com>
2023-02-04 12:19:53 +0800

    Added node generation functionality
    Needs more refining but more or less the flow of the generation is already here

--- Assets/Scripts/Managers/NodeGenerator.cs
+++ Assets/Scripts/Managers/NodeGenerator.cs
@@ -26,17 +26,58 @@ public class NodeGenerator : MonoBehaviour
     public Sprite ears_tri;
     public Sprite ears_sqr, ears_cir, ears_ova, ears_rec, ears_dia;
 
+    // While we haven't done the actual level implementations, any other necessary information about the level that the node generator also needs will be here. Maybe we could transfer it somewhere else or maybe reworked into another script, we'll figure it out.
+    [Header("Level Information")]
+    [Header("Tiers")]
+    [SerializeField] private int[] tier;
+
+    // This shouldn't be visible, only needed by the generator's algorithm
+    [Header("Algorithm Variables")]
+    private int currDepth;
+    private int currNodes = 0;
+
     // Start is called before the first frame update
     void Start()
     {
 	// Assume on level start/open the nodes will be generated.
-	GenerateNode();
+	init();
+	//GenerateNode();
     }
 
-    private void GenerateNode(){
+    private void init() {
+	Debug.Log("currDepth: " + currDepth);
+	Debug.Log("tier.Length: " + tier.Length);
+	//while(currDepth != tier.Length) {
+		GenerateNode(2, currDepth);
+		//currDepth++;
+	//}
+    }
+
+    private GameObject GenerateNode(int children, int currDepth){
 	// Assume that the object is being generated on a proper game object, else replace this with the proper parent gameobject.
 	GameObject nodeObj = Instantiate(nodeTemplate, this.transform);
-	NodeDummy node = nodeObj.GetComponent<NodeDummy>();
+	// Don't forget to replace with proper node script
+	Node node = nodeObj.GetComponent<Node>();
+	/*
 	node.SetTrait(TraitNames.Traits.Eyes, TraitNames.Attributes.TRIANGLE);
+	node.SetTrait(TraitNames.Traits.Nose, TraitNames.Attributes.SQUARE);
+	//node.SetTrait(TraitNames.Traits.Mouth, TraitNames.Attributes.TRIANGLE);
+	node.SetTrait(TraitNames.Traits.Ears, TraitNames.Attributes.CIRCLE);
+	*/
+	SetSprites(nodeObj);
+	if(currDepth < 3) {
+		for(int i = 0; i < children; i++){
+			var newNode = GenerateNode(2, currDepth+1);
+			node.Connections.Add(newNode);
+			newNode.GetComponent<Node>().Parent = nodeObj;
+		}
+	}
+	return nodeObj;
+    }
+
+    private void SetSprites(GameObject node){
+	// TODO: Add actual sprite replacements.
+	SpriteRenderer sprite = node.GetComponent<SpriteRenderer>();
+	sprite.color = Color.blue;
     }
 }
